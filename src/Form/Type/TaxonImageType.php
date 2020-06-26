f895c92b45cbd0f72aa3cb622b7f71a51e1f4bcb
David Robert <contact.davidrobert@gmail.com>
2020-06-26 11:22:53 +0200

    fix crop ration manager + taxon images bugs

--- src/Form/Type/TaxonImageType.php
+++ src/Form/Type/TaxonImageType.php
@@ -4,9 +4,18 @@ declare(strict_types=1);
 
 namespace Aropixel\SyliusAdminMediaPlugin\Form\Type;
 
+use App\Entity\Taxonomy\TaxonImage;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 final class TaxonImageType extends ImageType
 {
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults(array(
+            'data_class' => TaxonImage::class,
+        ));
+    }
 
     public function getBlockPrefix(): string
     {
